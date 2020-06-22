0a8d237048c89534519e904d03e828eb3f3cdfa7
Suraj Deshmukh <suraj@kinvolk.io>
2020-06-22 18:45:48 +0530

    kubelet: Remove hyperkube image
    Use image `quay.io/poseidon/kubelet` instead of hyperkube.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- assets/lokomotive-kubernetes/bootkube/variables.tf
+++ assets/lokomotive-kubernetes/bootkube/variables.tf
@@ -98,7 +98,7 @@ variable "container_images" {
     calico                  = "calico/node:v3.14.1"
     calico_cni              = "calico/cni:v3.14.1"
     calico_controllers      = "calico/kube-controllers:v3.14.1"
-    hyperkube               = "k8s.gcr.io/hyperkube:v1.18.4"
+    kubelet_image           = "quay.io/poseidon/kubelet:v1.18.4"
     coredns                 = "coredns/coredns:coredns-"
     pod_checkpointer        = "kinvolk/pod-checkpointer:d1c58443fe7d7d33aa5bf7d80d65d299be6e5847"
     kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.18.4"
